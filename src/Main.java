83d95d6e22db22d83e3bebb308c9c76e8b40202a
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-19 15:59:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,32 @@
 public class Main {
      public static void main(String[] args) {
 
-     /* your code here */
+        Node firstNode = new Node(40);
+        Node secNode = new Node(78);
+        Node thNode = new Node(97);
+        firstNode.next = secNode;
+        secNode.next = thNode;
+        System.out.println(firstNode.next.data);
 
+        Node currentNode = firstNode;
+        while (currentNode != null) {
+            currentNode.data += 2;
+            currentNode = currentNode.next;
+        }
+        System.out.println(firstNode.data);
+        System.out.println(firstNode.next.data);
+        System.out.println(firstNode.next.next.data);
+        
     }
+
 }
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+} 
